a25376bda7d3d123ceddbd40c7f8612282b6c63b
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2014-03-15 20:21:31 +0100

    wip

--- includes/nested_container.hpp
+++ includes/nested_container.hpp
@@ -11,6 +11,9 @@
 #include <initializer_list>
 #include <tuple>
 #include <typeinfo>
+#include <cstring>
+#include <cfloat>
+#include <limits>
 
 namespace nested_container {
 
@@ -654,7 +657,6 @@ using container = basic_container<>;
 namespace drivers {
 
 template<typename container_type> class json_sstream {
-  //using container_type = basic_container<T ...>;
   using base_visitor_type = typename container_type::const_visitor;
   using string_type = typename container_type::str_type;
   using ostream_type = std::basic_ostringstream<typename string_type::value_type, typename string_type::traits_type>;
@@ -712,6 +714,125 @@ template<typename container_type> class json_sstream {
   }
 };
 
+template <typename T> struct string_conversion_traits {
+  static size_t constexpr max_size = 0u;
+  static std::string const printf_code;
+};
+template <typename T> std::string const string_conversion_traits<T>::printf_code = "";
+
+template <> struct string_conversion_traits<int> {
+  static size_t constexpr max_size = std::numeric_limits<int>::digits + 1u;
+  static std::string const printf_code;
+};
+std::string const string_conversion_traits<int>::printf_code = "%d";
+
+template <> struct string_conversion_traits<unsigned int> {
+  static size_t constexpr max_size = std::numeric_limits<unsigned int>::digits;
+  static std::string const printf_code;
+};
+std::string const string_conversion_traits<unsigned int>::printf_code = "%u";
+
+template <> struct string_conversion_traits<size_t> {
+  static size_t constexpr max_size = std::numeric_limits<size_t>::digits;
+  static std::string const printf_code;
+};
+std::string const string_conversion_traits<size_t>::printf_code = "%lu";
+
+template <> struct string_conversion_traits<float> {
+  static size_t constexpr max_size = 3 + FLT_MANT_DIG - FLT_MIN_EXP;
+  static std::string const printf_code;
+};
+std::string const string_conversion_traits<float>::printf_code = "%f";
+
+template<typename container_type> class json{
+  using base_visitor_type = typename container_type::const_visitor;
+  using string_type = typename container_type::str_type;
+  using ostream_type = std::basic_ostringstream<typename string_type::value_type, typename string_type::traits_type>;
+
+  using key_type = typename container_type::key_type;
+  using map_type = typename container_type::map_type;
+  using vector_type = typename container_type::vector_type;
+  using float_type = typename container_type::float_type;
+  using int_type = typename container_type::int_type;
+  using uint_type = typename container_type::uint_type;
+
+  struct visitor_size : public base_visitor_type {
+    size_t size = 0u;
+    
+    virtual void apply(std::nullptr_t) override { size += 3u; }
+    
+    virtual void apply(map_type const& v) override {
+      size += 2u;  // {}
+      bool first = true;
+      for(std::pair<key_type, container_type> const& element : v) {
+        if (!first) ++size;
+        first = false;
+        size += 3u + element.first.size();
+        element.second.visit(*this);
+      }
+    }
+    
+    virtual void apply(vector_type const& v) override {
+      size += 2; // []
+      bool first = true;
+      for(container_type const& element : v) {
+        if (!first) ++size;
+        first = false;
+        element.visit(*this);
+      }
+    }
+    
+    virtual void apply(string_type const& v) override { size += 2u + v.size(); }
+    virtual void apply(float_type v) override {}
+    virtual void apply(int_type v) override {}
+    virtual void apply(uint_type v) override {}
+    virtual void apply(bool v) override {}
+  };
+  struct visitor_ostream : public base_visitor_type {
+    ostream_type output_stream;
+    
+    virtual void apply(std::nullptr_t) override { output_stream << "null"; }
+    
+    virtual void apply(map_type const& v) override {
+      output_stream << "{";
+      bool first = true;
+      for(std::pair<key_type, container_type> const& element : v) {
+        if (!first) output_stream << ",";
+        first = false;
+        output_stream << "\"" << element.first << "\":";
+        element.second.visit(*this);
+      }
+      output_stream << "}";
+    }
+    
+    virtual void apply(vector_type const& v) override {
+      output_stream << "[";
+      bool first = true;
+      for(container_type const& element : v) {
+        if (!first) output_stream << ",";
+        first = false;
+        element.visit(*this);
+      }
+      output_stream << "]";
+    }
+    
+    virtual void apply(string_type const& v) override { output_stream << "\"" << v << "\""; }
+    virtual void apply(float_type v) override { output_stream << v; }
+    virtual void apply(int_type v) override { output_stream << v; }
+    virtual void apply(uint_type v) override { output_stream << v; }
+    virtual void apply(bool v) override { output_stream << (v?"true":"false"); }
+  };
+
+ public:
+  string_type serialize(container_type const& container) const {
+    visitor_ostream visitor;
+    container.visit(visitor);
+    return visitor.output_stream.str();
+  }
+
+  container_type deserialize(string_type const& input) {
+  }
+};
 
 }
 
