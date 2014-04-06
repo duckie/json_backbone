46221c0595d42e76e159b699fb4fd177a026619d
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2014-04-06 18:57:42 +0200

    Typo

--- includes/nested_container/json.hpp
+++ includes/nested_container/json.hpp
@@ -1,5 +1,5 @@
-#ifndef NESTED_COMPILER_JSON
-#define NESTED_COMPILER_JSON
+#ifndef HEADER_GUARD_NESTED_COMPILER_JSON
+#define HEADER_GUARD_NESTED_COMPILER_JSON
 
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/phoenix.hpp>
