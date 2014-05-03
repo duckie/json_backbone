e64177b7db36de3113bf53307954d725c9ea0ce8
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2014-05-03 22:24:06 +0200

    File reorg

--- test/nested_container.cpp
+++ test/nested_container.cpp
@@ -2,7 +2,7 @@
 #include <nested_container/externalize.hpp>
 NESTED_CONTAINER_INSTANTIATE(NESTED_CONTAINER_CONTAINER_SIGNATURE());
 
-#include <nested_container/json.hpp>
-#include <nested_container/externalize_json.hpp>
+#include <nested_container/driver/json/json.hpp>
+#include <nested_container/driver/json/externalize_json.hpp>
 NESTED_CONTAINER_INSTANTIATE_JSON(NESTED_CONTAINER_CONTAINER_SIGNATURE());
 
