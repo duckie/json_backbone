7c8b50a7b62a97c88525364c0eb597fe537d324d
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2016-03-15 13:57:59 +0000

    Wip

--- include/json_backbone/unordered_base.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef JSON_BACKBONE_UNORDERED_BASE_HEADER
-#define JSON_BACKBONE_UNORDERED_BASE_HEADER
-#include <unordered_map>
-
-namespace json_backbone {
-template <typename key_type, typename value_type> using std_unordered_map_default_allocators = std::unordered_map<key_type, value_type>;
-}  // namespace json_backbone
-
-#endif  // JSON_BACKBONE_UNORDERED_BASE_HEADER
